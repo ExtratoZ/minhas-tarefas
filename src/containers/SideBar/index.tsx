d0282f823daf27b1e633fdccca6f7970b2dc85e8
Caetano <cacamelo98@hotmail.com>
2025-06-13 18:25:04 -0300

    initial Build

---
+++ src/containers/SideBar/index.tsx
@@ -0,0 +1,5 @@
+const SideBar = () => {
+  ;<aside>barra lateral</aside>
+}
+
+export default SideBar
---
+++ src/containers/ToDoList/index.tsx
@@ -0,0 +1,7 @@
+const ToDoList = () => {
+  ;<main>
+    <p>2 tarefas marcadas como: concluídas</p>
+  </main>
+}
+
+export default ToDoList
