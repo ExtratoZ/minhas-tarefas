d4c7555b975b75b956069bd77bd39fd3850f052c
Caetano <cacamelo98@hotmail.com>
2025-06-16 14:47:11 -0300

    build sidebar

--- src/containers/ToDoList/index.tsx
+++ src/containers/ToDoList/index.tsx
@@ -1,7 +1,9 @@
 const ToDoList = () => {
-  ;<main>
-    <p>2 tarefas marcadas como: concluídas</p>
-  </main>
+  return (
+    <main>
+      <p>2 tarefas marcadas como: concluídas</p>
+    </main>
+  )
 }
 
 export default ToDoList
